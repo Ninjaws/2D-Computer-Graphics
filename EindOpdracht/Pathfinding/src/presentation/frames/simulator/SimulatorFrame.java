97954b96b80b5c5b01b35ae2f00fd26e61ad18b2
Ninjaws <Ninjaws@protonmail.com>
2018-04-03 20:28:30 +0200

    [Updated] Simulator height to show the whole map properly

--- EindOpdracht/Pathfinding/src/presentation/frames/simulator/SimulatorFrame.java
+++ EindOpdracht/Pathfinding/src/presentation/frames/simulator/SimulatorFrame.java
@@ -23,7 +23,7 @@ public class SimulatorFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         int xMargin = 200;
-        int yMargin = 100;
+        int yMargin = 125;
 
         int width = Simulator.getInstance().getTileMap().getTiles()[0].length * Simulator.getInstance().getTileMap().getTileSize() + xMargin;
         int height = Simulator.getInstance().getTileMap().getTiles().length * Simulator.getInstance().getTileMap().getTileSize() + yMargin;
